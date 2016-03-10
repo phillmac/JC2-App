2dbda82ba73e2607ae0d5455b131e742abe0a4d5
Phillip <Phillip@192.168.1.50>
2016-03-11 00:39:48 +1100

    remove old plugin loader

--- src/main/java/me/tekkitcommando/jc2/JC2.java
+++ src/main/java/me/tekkitcommando/jc2/JC2.java
@@ -8,7 +8,6 @@ package me.tekkitcommando.jc2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import me.tekkitcommando.jc2.plugin.PluginLoader;
 import me.tekkitcommando.jc2.servers.ServerList;
 
 /**
@@ -28,7 +27,6 @@ public class JC2 {
     static IDManager idManager = new IDManager();
     static ServerList serverList = new ServerList();
     static List<JC2> appsList = new ArrayList();
-    PluginLoader pluginLoader;
 
     public static void main(String[] args) {
 
@@ -46,7 +44,6 @@ public class JC2 {
 
         Scanner scanner = new Scanner(System.in);
         String currentCommand;
-        pluginLoader = new PluginLoader(this);
     
         System.out.println("Info: Type /info to get the build version!");
         System.out.println("Info: Type /help to get a list of availble commands!");
