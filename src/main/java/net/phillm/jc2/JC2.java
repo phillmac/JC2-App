7b2afafec083e3453fde7c48c0d33d3a4e8cdaf8
Phillip <Phillip@192.168.1.50>
2016-04-09 17:56:19 +1000

    make / at start not mandatory

--- src/main/java/net/phillm/jc2/JC2.java
+++ src/main/java/net/phillm/jc2/JC2.java
@@ -8,6 +8,7 @@ package net.phillm.jc2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 import ro.fortsoft.pf4j.DefaultPluginManager;
 import ro.fortsoft.pf4j.PluginManager;
 import ro.fortsoft.pf4j.PluginWrapper;
@@ -52,43 +53,41 @@ public class JC2 {
         System.out.println("Info: Type /help to get a list of availble commands!");
 
         while (runLoop) { //loop untill /stop command
-            currentCommand = scanner.nextLine().toLowerCase();
+            currentCommand = StringUtils.removeStart(scanner.nextLine().toLowerCase(),"/");
 
-            if (currentCommand.contains("/info")) {
+            if (currentCommand.startsWith("info")) {
                 System.out.println("Build Version: " + version);
                 System.out.println("Website: " + website);
                 System.out.println("View on Github: " + github);
 
-            } else if (currentCommand.contains("/website") || currentCommand.contains("/site")) {
+            } else if (currentCommand.startsWith("website") || currentCommand.contains("/site")) {
                 System.out.println("Website: " + website);
 
-            } else if (currentCommand.contains("/help")) {
+            } else if (currentCommand.startsWith("help")) {
                 System.out.println("/Info: Gives Info Such as Build Version, Website, and Github Link");
                 System.out.println("/Website: Gives The Developers Website");
                 System.out.println("/Help: Lists Available Commands");
                 System.out.println("/Stop | /quit | /exit | /close: Close the console");
                 System.out.println("/Plugins: Lists Plugins");
 
-            } else if (currentCommand.contains("/stop") || currentCommand.contains("/quit") || currentCommand.contains("/exit") || currentCommand.contains("/close")) {
+            } else if (currentCommand.startsWith("stop") || currentCommand.startsWith("quit") || currentCommand.startsWith("exit") || currentCommand.startsWith("close")) {
                 System.out.println("Stopping");
                 runLoop = false;
-            } else if (currentCommand.contains("/plugins") || currentCommand.contains("/pl")) {
+            } else if (currentCommand.startsWith("plugins") || currentCommand.startsWith("pl")) {
                 List<PluginWrapper> startedPlugins = pluginManager.getStartedPlugins();
                 List<PluginWrapper> availablePlugins = pluginManager.getPlugins();
                 System.out.println("Available Plugins:");
                 
-                for (PluginWrapper plugin : availablePlugins) {
+                availablePlugins.stream().forEach((plugin) -> {
                     String pluginId = plugin.getDescriptor().getPluginId();
                     String pluginStatus = plugin.getPluginState().toString();
                     System.out.println(String.format("Name: '%s' Status: '%s'", pluginId, pluginStatus));
-                }
-                
-                //for (PluginWrapper plugin : startedPlugins) {
+                }); //for (PluginWrapper plugin : startedPlugins) {
                 //    String pluginId = plugin.getDescriptor().getPluginId();
                 //    System.out.println(String.format("Extensions added by plugin '%s':", pluginId));
                 //    for (String extension : pluginManager.getExtensionClassNames(pluginId)) {
                 //        System.out.println("   " + extension);
-                 //   }
+                //   }
                 //}
 
             }
